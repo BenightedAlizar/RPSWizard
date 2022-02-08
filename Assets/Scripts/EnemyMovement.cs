09c757a3a9457ddcee5d2d436fb2eceee9ad9a41
BenightedAlizar <benightedalizar@gmail.com>
2022-02-08 17:45:36 +0200

    Enemies fall faster, weakness system, knockback

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -10,7 +10,9 @@ public class EnemyMovement : MonoBehaviour
 
     public Transform player;
 
-    bool alive = true;
+    public bool alive = true;
+
+    public bool falling;
 
     private void Awake()
     {
@@ -20,21 +22,42 @@ public class EnemyMovement : MonoBehaviour
 
     void Update()
     {
-        if (alive)
+        if (alive && !falling)
         {
             Vector3 lookPosition = player.position - transform.position;
             Quaternion lookRotation = Quaternion.LookRotation(lookPosition, Vector3.up);
             float eulerY = lookRotation.eulerAngles.y;
             Quaternion rotation = Quaternion.Euler(0, eulerY, 0);
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * turnSpeed);
 
         }
 
+
+        Vector3 groundCheck = new Vector3(transform.position.x, transform.position.y -1 , transform.position.z);
+
+        if (!Physics.Linecast(transform.position, groundCheck))
+        {
+            falling = true;
+        }
+        else
+        {
+            falling = false;
+        }
     }
+
 
     private void FixedUpdate()
     {
-        rb.AddRelativeForce(Vector3.forward * movementSpeed);
+        if (alive && !falling)
+        {
+            rb.AddRelativeForce(Vector3.forward * movementSpeed);
+        }
+        else if (falling)
+        {
+            rb.AddRelativeForce(Vector3.down * 100);
+        }
     }
+
+
 
 }
